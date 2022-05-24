e417014a047789cc09ed2a6a04a3b8b8a336fa8d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-05-24 17:41:47 -0400

    add a few more test cases

--- code.test.js
+++ code.test.js
@@ -8,7 +8,7 @@ function getResult(original, given) {
   function recur(input) {
     return Object.keys(input).forEach(key => {
       path.push(key)
-      if(typeof input[key] === 'object') return recur(input[key])
+      if(typeof input[key] === 'object') recur(input[key])
       if(key === given) {
         founds.push(path.join('.'))
       }
@@ -22,11 +22,27 @@ function getResult(original, given) {
 }
 
 describe('daily code problem', () => {
-  it('should succesfully test', () => {
+  test('deep 1', () => {
     const input = {dos: 2, uno: 1, tres: {miel: false}}
 
     const result = getResult(input, 'miel')
 
     expect(result).toEqual(['tres.miel'])
+  })
+
+  test('deep 2', () => {
+    const input = {dos: 2, tres: {alpha: 'asd', beta: {miel: 'a'}}, uno: 1}
+
+    const result = getResult(input, 'miel')
+
+    expect(result).toEqual(['tres.beta.miel'])
+  })
+
+  test('multiple finds', () => {
+    const input = {dos: 2, tres: {alpha: 'asd', beta: {miel: 'a'}}, uno: 1, miel: 'lol', sin: {miel: 'm'}}
+
+    const result = getResult(input, 'miel')
+
+    expect(result).toEqual(['tres.beta.miel', 'miel', 'sin.miel'])
   })
 })
