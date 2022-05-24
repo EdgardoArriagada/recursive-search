abb08103f6318580a77ea0df316be199a058d487
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-05-24 17:36:39 -0400

    1st case

--- code.test.js
+++ code.test.js
@@ -1,13 +1,32 @@
-function getResult(input) {
-  return Object.entries(input).map(([k, v]) => ({[k]: v}))
+// const getResult = (input) => typeof input
+
+
+function getResult(original, given) {
+  const path = []
+  const founds = []
+
+  function recur(input) {
+    return Object.keys(input).forEach(key => {
+      path.push(key)
+      if(typeof input[key] === 'object') return recur(input[key])
+      if(key === given) {
+        founds.push(path.join('.'))
+      }
+      path.pop(key)
+    })
+  }
+
+  recur(original)
+
+  return founds
 }
 
 describe('daily code problem', () => {
   it('should succesfully test', () => {
-    const input = {dos: 2, uno: 1, tres: 3}
+    const input = {dos: 2, uno: 1, tres: {miel: false}}
 
-    const result = getResult(input)
+    const result = getResult(input, 'miel')
 
-    expect(result).toEqual([{dos: 2}, {uno: 1}, {tres: 3}])
+    expect(result).toEqual(['tres.miel'])
   })
 })
